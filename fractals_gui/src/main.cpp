3cf2639a943f910543d224a939592f65a4d4be59
jtremesaygues <jonathan.tremesaygues@gmail.com>
2013-10-31 23:00:33 +0100

    drawing fractal

--- fractals_gui/src/main.cpp
+++ fractals_gui/src/main.cpp
@@ -2,9 +2,26 @@
 
 #include <SFML/Window.hpp>
 
+#include <fractals/mandelbrot.h>
+
 int main(int argc, char const * argv[])
 {    
-    sf::Window window(sf::VideoMode(800, 600), "Fractals GUI");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Fractals GUI");
+    
+    sf::Image blankImage;
+    blankImage.create(1, 1, sf::Color::White);
+    
+    sf::Texture blankTexture;
+    blankTexture.loadFromImage(blankImage);
+    
+    sf::Sprite blankSprite;
+    blankSprite.setTexture(blankTexture);
+    blankSprite.setPosition(0, 0);
+    blankSprite.setScale(window.getSize().x, window.getSize().y);
+    
+    Mandelbrot mandelbrot;
+    mandelbrot.setCenter(sf::Vector2f(0.0f, 0.0f));
+    mandelbrot.setScaleFactor(2.0f);
     
     while (window.isOpen()) {
         sf::Event event;
@@ -13,6 +30,10 @@ int main(int argc, char const * argv[])
                 window.close();
             }
         }
+        
+        window.clear();
+        window.draw(blankSprite, &mandelbrot.getShader());
+        window.display();
     }
     
     return EXIT_SUCCESS;
